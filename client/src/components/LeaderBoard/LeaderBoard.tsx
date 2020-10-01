6e248b34670742e7715db229c967d8f89a8bc233
Mikaela McNamara <mcnamaramikaela@gmail.com>
2020-10-01 11:58:19 +1000

    Leaderboard card

--- client/src/components/LeaderBoard/LeaderBoard.tsx
+++ client/src/components/LeaderBoard/LeaderBoard.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import LeaderBoardHeader from '../../assets/LeaderBoardHeader.jpg';
+import LeaderBoardHeader from '../../assets/leaderboard.svg';
 
 import '../../App.css';
 import './LeaderBoard.css';
+
 import NavBar from '../common/Navbar/Navbar';
+import LeaderCard from '../common/LeaderCard/LeaderCard';
 
 const LeaderBoard = () => {
   return (
@@ -15,12 +17,17 @@ const LeaderBoard = () => {
         src={LeaderBoardHeader}
         alt='leaderboard-background'
       ></img>
-      <div className='favdeb'>
+
+      <div className='content'>
         <h1 className='leader-title'>LeaderBoard</h1>
-        <h1 className='title active'>Favours</h1>
-        <h1 className='title'>Debt</h1>
+        <h4 className='leader-subtitle'>
+          These users have the highest amount of favours blah
+        </h4>
       </div>
-      <div className='content'></div>
+      <LeaderCard />
+      <LeaderCard />
+      <LeaderCard />
+      <LeaderCard />
     </div>
   );
 };
