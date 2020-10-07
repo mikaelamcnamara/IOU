61dc1073fbc32d22970ddc80487a9c9308912717
JoeCode99 <dacalculator@gmail.com>
2020-10-07 13:41:02 +1100

    Fix TSX issue with progress bar

--- client/src/components/common/ProgressBar/ProgressBar.tsx
+++ client/src/components/common/ProgressBar/ProgressBar.tsx
@@ -1,38 +1,18 @@
 import React from 'react';
+import './ProgressBar.css';
 
 const ProgressBar = (props) => {
   const { bgcolor, completed } = props;
 
-  const containerStyles = {
-    height: 20,
-    width: '100%',
-    backgroundColor: '#e0e0de',
-    borderRadius: 50,
-    overflow: 'hidden',
-    margin: '0',
-  };
-
   const fillerStyles = {
-    height: '100%',
     width: `${completed}%`,
     backgroundColor: bgcolor,
-    borderRadius: 'inherit',
-    textAlign: 'right',
-    overflow: 'hidden',
-    margin: '0',
-  };
-
-  const labelStyles = {
-    padding: 5,
-    marginBottom: 5,
-    color: 'white',
-    fontWeight: 'bold',
   };
 
   return (
-    <div style={containerStyles}>
-      <div style={fillerStyles}>
-        <span style={labelStyles}></span>
+    <div className='progressbar-container'>
+      <div className='progressbar-filler' style={fillerStyles}>
+        <span className='progressbar-label'></span>
       </div>
     </div>
   );
