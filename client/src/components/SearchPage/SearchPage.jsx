2f676b53b9ef43542bafa105346e0a0bbeb67cbf
wasifhossainn <wasif.hossain@hotmail.com>
2020-10-17 13:28:23 +1100

    ADDED FAILED FOUNDED FAVOUR, MADE UI DYNAMIC, FIXED BUGS

--- client/src/components/SearchPage/SearchPage.jsx
+++ client/src/components/SearchPage/SearchPage.jsx
@@ -5,7 +5,9 @@ import './SearchPage.css';
 import Pagination from '../common/Pagination/Pagination';
 import queryString from 'query-string';
 import Cards from '../common/Cards/Cards';
+import Lottie from 'react-lottie';
 import favoursList from '../common/dummyData.json';
+import searchAnimation from '../../assets/Searching.json';
 
 const SearchPage = props => {
   const searchValue = queryString.extract(props.location.search);
@@ -13,6 +15,16 @@ const SearchPage = props => {
   const [favours, setFavours] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(10);
+
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: searchAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
 
 
   useEffect(() => {
@@ -31,18 +43,32 @@ const SearchPage = props => {
 
   const paginate = pageNumbers => setCurrentPage(pageNumbers);
 
+
+  const renderCards = () => {
+    return (<div className="search-bg">
+      {currentPosts.map((favours, index) => {
+        return <AvatarCard key={index} {...favours} />
+      })}
+      <Pagination postsPerPage={postsPerPage} totalAvatarCards={filteredFavours.length} paginate={paginate} />
+    </div>)
+  }
+
   return (
     <>
       <div>
         <NavBar />
-        <h1 className="search-results" >{`Search Results: ${searchValue}`} </h1>
-        <p className="search-number-results"> {`Showing ${filteredFavours.length} results for ${searchValue}`} </p>
-        <div className="search-bg">
-          {currentPosts.map((favours, index) => {
-            return <AvatarCard key={index} {...favours} />
-          })}
-          <Pagination postsPerPage={postsPerPage} totalAvatarCards={filteredFavours.length} paginate={paginate} />
-        </div>
+        <h1 className="search-results" >{(searchValue === "") ? "All of the favours" : `Search Results: ${searchValue}`} </h1>
+        <p className="search-number-results"> {(searchValue === "") ? `Showing all ${filteredFavours.length} favours` : `Showing ${filteredFavours.length} results for ${searchValue}`} </p>
+        {
+          (filteredFavours.length === 0) ?
+            <div className="failed-content">
+              <div className='search-fail-animation'>
+                <Lottie options={defaultOptions} height={700} width={700} />
+              </div>
+              <h1 className="failed-search">Oops...we could not find that favour.</h1>
+            </div> : renderCards()
+        }
+
 
       </div>
     </>
