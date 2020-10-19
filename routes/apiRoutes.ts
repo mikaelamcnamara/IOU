68983f2afefe035ba023b1b50d074cea316fe4ba
JoeCode99 <dacalculator@gmail.com>
2020-10-19 21:22:27 +1100

    Slight updates

--- routes/apiRoutes.ts
+++ routes/apiRoutes.ts
@@ -1,6 +1,7 @@
 export {}; //trick TS into accepting below imports
 const mongoose = require('mongoose');
 const Favour = mongoose.model('favours');
+const User = mongoose.model('users');
 module.exports = (app) => {
   app.post('/api/logout', (req: any, res: any) => {
     req.logout(); //kills cookie
@@ -27,8 +28,7 @@ module.exports = (app) => {
     favour.category = category;
     favour.points = points;
     favour.date = date;
-
-    favour.save((err: any) => {
+    favour.save((err: any, favour: any) => {
       if (err) {
         console.log(err);
         return res.send({
@@ -36,6 +36,7 @@ module.exports = (app) => {
           message: 'Server error',
         });
       } else {
+        User.findByIdAndUpdate(req.session.passport.user, {$push: {"myFavours": favour._id}}, {safe: true, new: true, upsert: true}, function(err) { console.log(err); });
         return res.send({
           success: true,
           message: 'Favour created!',
